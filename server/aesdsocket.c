3064415b919b12a233eb05d71f43d735b76671bb
Benjamin Lappinga <benjaminl746@gmail.com>
2026-07-13 21:47:20 +0000

    assignment-6-part1-complete

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,11 +12,36 @@
 #include <syslog.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <pthread.h>
 
 #define PORT 9000
 #define BUF_SIZE 1024
 
 volatile sig_atomic_t terminate = 0;
+volatile sig_atomic_t writeTime = 0;
+
+typedef struct threadList {
+    pthread_t thread_id;
+    int client_fd;
+    struct threadList *next_thread;
+} threadList;
+
+struct {
+    FILE *append_fd;
+    FILE *readback_fd;
+} fileFDs;
+
+pthread_mutex_t fd_lock;
+threadList *threads = NULL;
+
+void * thread_funct(void *fd);
+void timer_write(int signum)
+{
+    if (signum == SIGALRM)
+    {
+        writeTime = 1;
+    }
+}
 
 static void handler(int signum)
 {
@@ -102,17 +127,35 @@ int main(int argc, char *argv[])
     }
 
     //open fds
-    FILE *append_fd;
-    FILE *readback_fd;
-    append_fd = fopen("/var/tmp/aesdsocketdata", "w");
-    readback_fd = fopen("/var/tmp/aesdsocketdata", "r");
-    uint8_t *datBuf = malloc(sizeof(uint8_t) * BUF_SIZE);
-    uint8_t *datBuf_echo = malloc(sizeof(uint8_t) * BUF_SIZE);
+    fileFDs.append_fd = fopen("/var/tmp/aesdsocketdata", "w");
+    fileFDs.readback_fd = fopen("/var/tmp/aesdsocketdata", "r");
+
+    pthread_mutex_init(&fd_lock, NULL);
+    threadList *curThread;
+    struct sigaction saTime;
+    memset(&saTime, 0, sizeof(saTime));
+    saTime.sa_handler = timer_write;
+    sigemptyset(&saTime.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGALRM, &saTime, NULL);
+    alarm(10);
     while (!terminate)
     {
+        if (writeTime)
+        {
+            time_t now = time(NULL);
+            struct tm tm_info;
+            localtime_r(&now, &tm_info);
+            char timebuf[64];
+            strftime(timebuf, sizeof(timebuf), "%a, %d %b %Y %H:%M:%S %z", &tm_info);
+            pthread_mutex_lock(&fd_lock);
+            fprintf(fileFDs.append_fd, "timestamp:%s\n", timebuf);
+            fflush(fileFDs.append_fd);
+            pthread_mutex_unlock(&fd_lock);
+            alarm(10);
+        }
         struct sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
-
         int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
         if (client_fd < 0)
         {
@@ -123,54 +166,87 @@ int main(int argc, char *argv[])
             perror("accept");
             continue;
         }
-
+        if (threads == NULL)
+        {
+            threads = malloc(sizeof(threadList));
+            curThread = threads;
+        }
+        else 
+        {
+            curThread = threads;
+            while (curThread->next_thread != NULL)
+            {
+                curThread = curThread->next_thread;
+            }
+            curThread->next_thread = malloc(sizeof(threadList));
+            curThread = curThread->next_thread;
+        }
+        curThread->client_fd = client_fd;
+        curThread->next_thread = NULL;
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
-        ssize_t bytes_recv;
-        while ((bytes_recv = read(client_fd, datBuf, BUF_SIZE)) > 0)
-{
-    ssize_t offset = 0;
-
-    while (offset < bytes_recv)
-    {
-        uint8_t *nl = memchr(datBuf + offset, '\n', bytes_recv - offset);
-
-        if (nl != NULL)
-        {
-            size_t chunk_len = (nl - (datBuf + offset)) + 1; // include the '\n'
-            fwrite(datBuf + offset, sizeof(uint8_t), chunk_len, append_fd);
-            fflush(append_fd);
-
-            fseek(readback_fd, 0, SEEK_SET);
-            size_t bytes_read;
-            while ((bytes_read = fread(datBuf_echo, sizeof(uint8_t), BUF_SIZE, readback_fd)) > 0)
-            {
-                ssize_t total_written = 0;
-                while (total_written < (ssize_t)bytes_read)
-                {
-                    ssize_t n = write(client_fd, datBuf_echo + total_written, bytes_read - total_written);
-                    if (n < 0) { perror("write"); break; }
-                    total_written += n;
-                }
-            }
-
-            offset += chunk_len;
-        }
-        else
-        {
-            fwrite(datBuf + offset, sizeof(uint8_t), bytes_recv - offset, append_fd);
-            fflush(append_fd);
-            offset = bytes_recv;
-        }
+        pthread_create(&(curThread->thread_id), NULL, thread_funct, &(curThread->client_fd));
     }
-}
-        close(client_fd);
+    while (threads != NULL)
+    {
+        curThread = threads;
+        pthread_join(threads->thread_id, NULL);
+        threads = threads->next_thread;
+        free(curThread);
     }
     close(server_fd);
-    fclose(append_fd);
-    fclose(readback_fd);
-    free(datBuf);
-    free(datBuf_echo);
+    fclose(fileFDs.append_fd);
+    fclose(fileFDs.readback_fd);
+    pthread_mutex_destroy(&fd_lock);
 }
 
+void *thread_funct(void *fd)
+{
+    int client_fd = *(int *)fd;
+    uint8_t *datBuf = malloc(sizeof(uint8_t) * BUF_SIZE);
+    uint8_t *datBuf_echo = malloc(sizeof(uint8_t) * BUF_SIZE);
+    ssize_t bytes_recv;
+    while ((bytes_recv = read(client_fd, datBuf, BUF_SIZE)) > 0)
+    {
+        ssize_t offset = 0;
+        while (offset < bytes_recv)
+        {
+            uint8_t *nl = memchr(datBuf + offset, '\n', bytes_recv - offset);
+            if (nl != NULL)
+            {
+                size_t chunk_len = (nl - (datBuf + offset)) + 1; // include the '\n'
+                pthread_mutex_lock(&fd_lock);
+                fwrite(datBuf + offset, sizeof(uint8_t), chunk_len, fileFDs.append_fd);
+                fflush(fileFDs.append_fd);
+
+                fseek(fileFDs.readback_fd, 0, SEEK_SET);
+                size_t bytes_read;
+                while ((bytes_read = fread(datBuf_echo, sizeof(uint8_t), BUF_SIZE, fileFDs.readback_fd)) > 0)
+                {
+                    ssize_t total_written = 0;
+                    while (total_written < (ssize_t)bytes_read)
+                    {
+                        ssize_t n = write(client_fd, datBuf_echo + total_written, bytes_read - total_written);
+                        if (n < 0) { perror("write"); break; }
+                        total_written += n;
+                    }
+                }
+                pthread_mutex_unlock(&fd_lock);
+                offset += chunk_len;
+            }
+            else
+            {
+                pthread_mutex_lock(&fd_lock);
+                fwrite(datBuf + offset, sizeof(uint8_t), bytes_recv - offset, fileFDs.append_fd);
+                fflush(fileFDs.append_fd);
+                pthread_mutex_unlock(&fd_lock);
+                offset = bytes_recv;
+            }
+        }
+    }
+    close(client_fd);
+    free(datBuf);
+    free(datBuf_echo);
+    return NULL;
+}
